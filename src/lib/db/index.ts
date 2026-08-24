a2904c354f07c3405ac15c96af0b1d3a04c1f34e
Kilian Stelling <kilianguntermann@gmx.de>
2026-08-24 19:12:48 +0200

    set last env var in db index.ts

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
+import { env } from '$env/dynamic/private';
 
-const client = postgres('postgres://postgres:postgres@localhost:5432/lebenslauf');
+const client = postgres(env.DATABASE_URL);
 
 export const db = drizzle(client, { schema });
